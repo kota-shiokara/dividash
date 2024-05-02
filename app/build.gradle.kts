7b2a49e22ec4194e297344e667707e5582829ca3
kota-shiokara <ikanoshiokara.fun@gmail.com>
2024-05-02 18:58:14 +0900

    v1.0.0-rc2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "jp.ikanoshiokara.dividash"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 3
+        versionName = "1.0.0-rc2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
