398af4d0022faa62b5e11bbc126c44d1db2b1ad9
kota-shiokara <ikanoshiokara.fun@gmail.com>
2025-01-14 03:01:07 +0900

    koin viewmodel: 別のパッケージにいったviewmodel関数を参照するようにした

--- app/src/main/java/jp/ikanoshiokara/dividash/ui/screen/ViewModelModules.kt
+++ app/src/main/java/jp/ikanoshiokara/dividash/ui/screen/ViewModelModules.kt
@@ -2,7 +2,7 @@ package jp.ikanoshiokara.dividash.ui.screen
 
 import jp.ikanoshiokara.dividash.ui.screen.main.MainViewModel
 import jp.ikanoshiokara.dividash.ui.screen.settings.SettingsViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModules =
