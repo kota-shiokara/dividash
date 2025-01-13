6a6685066a40de751de6cc2f43258e10ae7ac178
kota-shiokara <ikanoshiokara.fun@gmail.com>
2025-01-14 03:39:00 +0900

    renovate major kotlin monorepo: 祝Kotlin2.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.jetbrainsKotlinSerialization) apply false
+    alias(libs.plugins.jetbrainsKotlinCompose) apply false
     alias(libs.plugins.kotlinter) apply false
 }
