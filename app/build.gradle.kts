9123dd0cfa5bc55c237221f493231e3c208a15db
kota-shiokara <ikanoshiokara.fun@gmail.com>
2025-06-21 05:21:44 +0900

    Dynamic Color: Versionを巻き戻した

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "jp.ikanoshiokara.dividash"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 2
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
